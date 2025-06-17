6944ad75cca397a69f993308fbde67971aa099be
Zapwa Dev <zapwadev@gmail.com>
2025-06-17 22:55:17 +0700

    chore: update dependencies and devDependencies in package.json
    - Added new dependencies: class-variance-authority, clsx, lucide-react, tailwind-merge
    - Updated devDependencies: @eslint/js, @stylistic/eslint-plugin, eslint, eslint-plugin-react, gh-pages, globals, tw-animate-css, typescript-eslint

---
+++ eslint.config.js
@@ -0,0 +1,28 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+import pluginReact from 'eslint-plugin-react'
+import { defineConfig } from 'eslint/config'
+import stylistic from '@stylistic/eslint-plugin'
+
+export default defineConfig([
+  { ignores: ['.wxt/', 'dist/'] },
+  { files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'], plugins: { js }, extends: ['js/recommended'] },
+  { files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'], languageOptions: { globals: globals.browser } },
+  ...tseslint.configs.recommended,
+  {
+    ...pluginReact.configs.flat.recommended,
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+  stylistic.configs.recommended,
+  {
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
+  },
+])
